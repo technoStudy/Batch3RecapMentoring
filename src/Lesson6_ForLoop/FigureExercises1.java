6e281105a0f7edc1dfc5ed7c69d6930bbd5b5c3f
Muharrem <muharremustun@gmail.com>
2020-12-21 17:56:55 -0500

    Lesson 6 - for loop - Part 3

--- src/Lesson6_ForLoop/FigureExercises1.java
+++ src/Lesson6_ForLoop/FigureExercises1.java
@@ -2,14 +2,61 @@ package Lesson6_ForLoop;
 
 public class FigureExercises1 {
 
-//          Print the following figure using loops
-//          *
+
+    public static void main(String[] args) {
+
+/*//          *
 //          **
 //          ***
 //          ****
 //          *****
+        for (int row = 1; row <= 5; row++) {
+            for (int star = 1; star <= row; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
 
-    public static void main(String[] args) {
+
+
+//        System.out.println();
+
+
+/*
+ *****
+ ****
+ ***
+ **
+ *
+
+        for (int row = 5; row >= 1; row--) {
+            for (int star = 1; star <= row; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+ */
+
+
+//        System.out.println();
+
+
+
+//          *****
+//           ****
+//            ***
+//             **
+//              *
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= 5; col++) {
+                if(col < row)
+                System.out.print(" ");
+                else
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
     }
 
--- src/Lesson6_ForLoop/NestedForLoops.java
+++ src/Lesson6_ForLoop/NestedForLoops.java
@@ -1,22 +1,18 @@
 package Lesson6_ForLoop;
 
+import java.util.Arrays;
+
 public class NestedForLoops {
     public static void main(String[] args) {
         /*
-        - nested loops with examples
-    - print all the prime numbers less than a given number (inclusive)
-    - print all the possible letter pairs by using the letters a, b, c and d
-    - print all the possible number pairs by using numbers between 1 and 5 (inclusive)
-      where second number is bigger than the first number of the pair.
 - Labeling loops and using break statement with labels
  */
 
+/*         - print all the prime numbers less than a given number (inclusive)
         int num = 9;
-
         if(num >= 2) {
             System.out.print("2, ");
         }
-
         for (int i = 3; i <= num; i++) {
             int number = i;
 
@@ -30,10 +26,57 @@ public class NestedForLoops {
             if (isPrime) {
                 System.out.print(number + ", ");
             }
+        }*/
 
 
+/*        //     - print all the possible letter pairs by using the letters a, b, c and d
+        for (char row = 'a'; row <= 'd' ; row++) {
+            for (char col = 'a'; col <='d' ; col++) {
+                System.out.print(String.valueOf(row) + col + " ");
+            }
+            System.out.println();
+        }*/
+
+/*//print all the possible number pairs by using numbers between 1 and 5 (inclusive)
+        for (int i = 1; i <= 5 ; i++) {
+            for (int j = 1; j < 6 ; j++) {
+                System.out.print("(" + i + ", " + j + ") ");
+            }
+            System.out.println();
+        }*/
+
+//    print all the possible number pairs by using numbers between 1 and 5 (inclusive)
+//    where second number is bigger than the first number of the pair.
+
+/*//        1st way:
+        for (int i = 1; i <= 5 ; i++) {
+            for (int j = 1; j <= 5 ; j++) {
+                if(j > i)
+                System.out.print("(" + i + ", " + j + ") ");
+            }
+            System.out.println();
         }
 
+        System.out.println("------------------------------");
+//        2. way: (Preferred solution)
+         for (int i = 1; i <= 5 ; i++) {
+            for (int j = i+1; j <= 5 ; j++) {
+                System.out.print("(" + i + ", " + j + ") ");
+            }
+            System.out.println();
+        }*/
+
+
+/*// Labeling loops and using break statement with labels
+        Distaki : for (int i = 1; i <= 5 ; i++) {
+            icteki: for (int j = i+1; j <= 5 ; j++) {
+                if(i == 2 && j == 4){
+                    break Distaki;
+                }
+                System.out.print("(" + i + ", " + j + ") ");
+            }
+            System.out.println();
+        }*/
 
     }
 }
